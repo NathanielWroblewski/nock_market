4ca1bdec58aeb986781fae0a7ffe5a16f4b2fea4
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-10-12 23:45:43 -0700

    Render chart

--- nockmarket.js
+++ nockmarket.js
@@ -3,8 +3,10 @@
 var exchangeData = {}
   , exch = require('./lib/exchange')
   , nocklib = require('./lib/nocklib')
+  , db = require('./lib/db')
   , timeFloor = 500
-  , timeRange = 1000;
+  , timeRange = 1000
+  , express = require('express');
 
 function submitRandomOrder() {
   var ord = nocklib.generateRandomOrder (exchangeData);
@@ -34,6 +36,47 @@ function submitRandomOrder() {
   }
 }
 
+var app = express.createServer();
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.set('view options', {
+  layout: false
+})
+
+app.get('/', function(req, res) {
+  res.render('chart');
+});
+
+app.get('/api/trades', function(req, res) {
+  db.find('transactions'
+    , { init: { $exists: true } }
+    , 100, function(err, trades) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    var json = [];
+    var lastTime = 0;
+    trades.reverse().forEach(function(trade) {
+      var date = new Date(parseInt(trade._id
+        .toString()
+        .substring(0,8), 16) * 1000);
+      var dataPoint = [date.getTime(), trade.price];
+      if (date - lastTime > 1000)
+        json.push(dataPoint);
+      lastTime = date;
+    });
+
+    res.json(json);
+  });
+});
+
 db.open(function() {
   submitRandomOrder();
+  app.listen(3000);
 });
